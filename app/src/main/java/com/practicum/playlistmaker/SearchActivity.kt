5a8a25fae55fac75167b172cabf659138da1c564
Viraremi <vlad.zaharov.1718@gmail.com>
2024-09-22 14:42:47 +0400

    Спринт 9 (3/4)

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,14 +1,46 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val btn_clear = findViewById<ImageView>(R.id.search_clear)
+        val search_bar = findViewById<EditText>(R.id.search_bar)
+
+        btn_clear.setOnClickListener{
+            search_bar.setText("")
+        }
+
+        val searchTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //none
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                btn_clear.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //none
+            }
+        }
+        search_bar.addTextChangedListener(searchTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
     }
 }
