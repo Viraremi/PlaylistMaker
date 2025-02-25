dfac240c20593b19512d1b2e76577b383982b139
Viraremi <vlad.zaharov.1718@gmail.com>
2025-02-25 14:05:03 +0400

    RootActivity и правки манифеста для новой активити

---
+++ app/src/main/java/com/practicum/playlistmaker/RootActivity.kt
@@ -0,0 +1,25 @@
+package com.practicum.playlistmaker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+
+class RootActivity : AppCompatActivity(R.layout.activity_root) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportFragmentManager.commit {
+
+        }
+    }
+}
