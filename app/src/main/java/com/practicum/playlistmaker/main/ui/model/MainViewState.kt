785f940eb70dc7d69084042b8a049908f7e13c8b
Viraremi <vlad.zaharov.1718@gmail.com>
2025-02-25 17:13:36 +0400

    Реализация BottomNavigationView

--- app/src/main/java/com/practicum/playlistmaker/main/ui/model/MainViewState.kt
+++
@@ -1,9 +0,0 @@
-package com.practicum.playlistmaker.main.ui.model
-
-import com.practicum.playlistmaker.main.ui.viewModel.MainViewModel
-
-sealed interface MainViewState {
-    object SEARCH : MainViewState
-    object LIBRARY : MainViewState
-    object SETTINGS : MainViewState
-}
