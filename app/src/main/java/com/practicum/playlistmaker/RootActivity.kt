712894c11ddd6033e5a7c29cb2d3d4c0b7005181
Viraremi <vlad.zaharov.1718@gmail.com>
2025-02-25 18:17:26 +0400

    В какойто момент edgeToEdge везде поломался и я его вырезал. Сейчас просто вернул его в root и все заработало ._.

--- app/src/main/java/com/practicum/playlistmaker/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/RootActivity.kt
@@ -1,7 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,6 +13,13 @@ class RootActivity : AppCompatActivity(R.layout.activity_root) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.root_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
